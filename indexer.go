bd1d837652eefb7a3b6d15e43091a94f5cb5e3eb
Adrien <adrien@arccoza.com>
2020-02-05 23:58:27 +0900

    Update segmenter, add SplitterGen to gen splitter fn with options

--- indexer.go
+++ indexer.go
@@ -1,42 +1,45 @@
 package main
 
 import (
-	// "github.com/go-ego/gse"
 	// "github.com/bbalet/stopwords"
 	"github.com/davecgh/go-spew/spew"
 	"bufio"
 	"strings"
+	// "fmt"
 	"github.com/blevesearch/segment"
 )
 
 func StemText(text string) string {
-	// var seg gse.Segmenter
-
-	// seg.LoadDict()
-	// words := seg.Segment([]byte(text))
-	// // words := gse.SplitTextToWords([]byte(text))
-	// count:= len(words)
-	// spew.Dump(text, words)
-
-	strings.NewReader(text)
+	// strings.NewReader(text)
 
 	scanner := bufio.NewScanner(strings.NewReader(text))
-	scanner.Split(segment.SplitWords)
-	count := 0
+	scanner.Split(SplitterGen([5]bool{true, false, false, false, false}))
 	for scanner.Scan() {
-		count++
-		tokenBytes := scanner.Bytes()
-		spew.Dump(tokenBytes)
+		tokenBytes := scanner.Text()
+		spew.Dump(tokenBytes, len(tokenBytes))
 	}
 	if err := scanner.Err(); err != nil {
-		spew.Dump(err)
+		spew.Dump("err:", err)
 	}
-
-	spew.Dump(count)
 
 	return ""
 }
 
-func StemWord(word string) string {
-	return ""
+func SplitterGen(skip [5]bool) func([]byte, bool) (int, []byte, error) {
+	return func(data []byte, atEOF bool) (int, []byte, error) {
+		advance := 0
+		Again:
+			adv, tok, typ, err := segment.SegmentWords(data, atEOF)
+			advance += adv
+		
+		if skip[typ] && err == nil && tok != nil && adv > 0 {
+			if atEOF {
+				data = data[adv:]
+				goto Again
+			}
+			tok = nil
+		}
+
+		return advance, tok, err
+	}
 }
