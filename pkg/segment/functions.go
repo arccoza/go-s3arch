a07a6336b83bb61f1a714fa6787b02dd3a8ac312
Adrien <adrien@arccoza.com>
2020-02-15 03:11:20 +0900

    Remove inline ranges

--- pkg/segment/functions.go
+++ pkg/segment/functions.go
@@ -22,23 +22,6 @@ func graphemeSplit(data []byte, atEOF bool) (int, []byte, error) {
 		return 0, nil, nil
 	}
 
-	var Consonant_Prefixed = &unicode.RangeTable{
-	  R32: []unicode.Range32{
-	    {0x111C2, 0x111C3, 1},
-	    {0x11A3A, 0x11A3A, 1},
-	    {0x11A84, 0x11A89, 1},
-	  },
-	}
-
-	var Consonant_Preceding_Repha = &unicode.RangeTable{
-	  R16: []unicode.Range16{
-	    {0x0D4E, 0x0D4E, 1},
-	  },
-	  R32: []unicode.Range32{
-	    {0x11D46, 0x11D46, 1},
-	  },
-	}
-
 	const (
 		Char = hangul.L << iota
 		Extend
