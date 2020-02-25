710397c98bd7e36565bf812dbeccc4543b7c29a4
Adrien <adrien@arccoza.com>
2020-02-25 18:21:05 +0900

    WIP update segement graphemes approach

--- pkg/segment/functions.go
+++ pkg/segment/functions.go
@@ -9,7 +9,7 @@ import (
 	"unicode"
 	"unicode/utf8"
 
-	"github.com/arccoza/go-s3arch/pkg/hangul"
+	// "github.com/arccoza/go-s3arch/pkg/hangul"
 	// "github.com/davecgh/go-spew/spew"
 )
 
@@ -20,9 +20,10 @@ func Graphemes(text io.Reader) *bufio.Scanner {
 }
 
 const (
-	Char = hangul.LVT << (iota + 1)
-	Control_CR
-	Control_LF
+	// Char = hangul.LVT << (iota + 1)
+	// CR = 1 << (iota + 1)
+	CR = (iota + 2)
+	LF
 	Control
 	Extend
 	RI
@@ -31,7 +32,7 @@ const (
 	ExtPict
 	Extend_ExtCccZwj
 	ZWJ_ExtCccZwj
-	Other = 0
+	Other = 1
 	None = 0
 )
 
@@ -47,12 +48,12 @@ func graphemeSplit(data []byte, atEOF bool) (int, []byte, error) {
 		return 0, nil, nil
 	}
 
-	// prev := None
+	prev := None
 	adv, _, stp := 0, 0, 0
 	// lth := adv - skp
 	buf := data[:]
 	fmt.Println("-------------------", atEOF)
-	gr := grStart
+	fmt.Println(CR, LF)
 
 	for i, take := 0, 1; i < take && len(buf) > 0; i++ {
 		r, s := utf8.DecodeRune(buf)
@@ -60,7 +61,7 @@ func graphemeSplit(data []byte, atEOF bool) (int, []byte, error) {
 		adv += s
 		buf = buf[stp:]
 
-		if gr = gr(r); gr == nil {
+		if prev = firstOf(r, breakTable[prev]...); prev == -1 {
 			fmt.Println("break")
 			// stp = 0
 			break
@@ -69,163 +70,107 @@ func graphemeSplit(data []byte, atEOF bool) (int, []byte, error) {
 			// spew.Dump(gr)
 			take++
 		}
+
+		// fmt.Println(firstOf(r, breakTable[prev]...))
 	}
 
 	adv -= stp
 	return adv, data[:adv], nil
 }
 
-type grFn func(r rune) grFn
-type isFn = func(r rune) bool
+var breakTable = [][]func(rune) int{
+	// Unknown
+	{isCR, isLF, isControl, isExtend, isRI, isPrepend, isSpacingMark, isExtend_ExtCccZwj, isZWJ_ExtCccZwj, isOther,},
+	// Other
+	{isExtend, isSpacingMark, isExtend_ExtCccZwj, isZWJ_ExtCccZwj,},
+	// CR
+	{isLF,},
+	// LF
+	{},
+	// Control
+	{},
+	// Extend
+	{isExtend, isSpacingMark, isExtend_ExtCccZwj, isZWJ_ExtCccZwj,},
+	// RI
+	{isExtend, isRI, isSpacingMark, isExtend_ExtCccZwj, isZWJ_ExtCccZwj,},
+	// Prepend
+	{},
+	// Other & Extend & RI & Prepend & SpacingMark & L & V & T & LV & LVT & ExtPict & Extend_ExtCccZwj & ZWJ_ExtCccZwj,
+	// SpacingMark
+	{isExtend, isSpacingMark, isExtend_ExtCccZwj, isZWJ_ExtCccZwj,},
+}
 
-func ifttt(r rune, fns ...interface{}) grFn {
-	fmt.Println("ifttt")
-	for i := 0; i < len(fns); i += 2 {
-		fmt.Println(i)
-		f := fns[i].(isFn)
-		if f(r) {
-			return fns[i + 1].(func(rune) grFn)
+func firstOf(r rune, fns ...func(rune) int) int {
+	for _, fn := range fns {
+		if v := fn(r); v > 0 {
+			return v
 		}
 	}
-	return nil
+	return -1
 }
 
-func grStart(r rune) grFn {
-	fmt.Println("grStart")
-	return ifttt(r,
-		isCR, grCR,
-		isLF, grLF,
-		isControl, grControl,
-		isExtend, grExtend,
-		isRI, grRI,
-		isPrepend, grPrepend,
-		isSpacingMark, grSpacingMark,
-		isExtend_ExtCccZwj, grExtend_ExtCccZwj,
-		isZWJ_ExtCccZwj, grZWJ_ExtCccZwj,
-		isOther, grOther)
-}
-
-func grOther(r rune) grFn {
-	fmt.Println("grOther")
-	return ifttt(r,
-		isExtend, grExtend,
-		isSpacingMark, grSpacingMark,
-		isExtend_ExtCccZwj, grExtend_ExtCccZwj,
-		isZWJ_ExtCccZwj, grZWJ_ExtCccZwj)
-}
-
-func grCR(r rune) grFn {
-	fmt.Println("grCR")
-	return ifttt(r, isLF, grLF)
-}
-
-func grLF(r rune) grFn {
-	fmt.Println("grLF")
-	return nil
-}
-
-func grControl(r rune) grFn {
-	return nil
-}
-
-func grExtend(r rune) grFn {
-	return ifttt(r,
-		isExtend, grExtend,
-		isSpacingMark, grSpacingMark,
-		isExtend_ExtCccZwj, grExtend_ExtCccZwj,
-		isZWJ_ExtCccZwj, grZWJ_ExtCccZwj)
-}
-
-func grRI(r rune) grFn {
-	fmt.Println("grRI")
-	return ifttt(r,
-		isExtend, grExtend,
-		isRI, grRI,
-		isSpacingMark, grSpacingMark,
-		isExtend_ExtCccZwj, grExtend_ExtCccZwj,
-		isZWJ_ExtCccZwj, grZWJ_ExtCccZwj)
-}
-func grPrepend(r rune) grFn {
-	return ifttt(r,
-		isExtend, grExtend,
-		isRI, grRI,
-		isSpacingMark, grSpacingMark,
-		isExtend_ExtCccZwj, grExtend_ExtCccZwj,
-		isZWJ_ExtCccZwj, grZWJ_ExtCccZwj,
-		isOther, grOther)
-}
-func grSpacingMark(r rune) grFn {
-	fmt.Println("grSpacingMark")
-	return ifttt(r,
-		isExtend, grExtend,
-		isSpacingMark, grSpacingMark,
-		isExtend_ExtCccZwj, grExtend_ExtCccZwj,
-		isZWJ_ExtCccZwj, grZWJ_ExtCccZwj)
-}
-func grExtPict(r rune) grFn {
-	return nil
-}
-func grExtend_ExtCccZwj(r rune) grFn {
-	return nil
-}
-func grZWJ_ExtCccZwj(r rune) grFn {
-	return nil
-}
-
-func isOther(r rune) bool {
-	return true
-}
-
-func isCR(r rune) bool {
-	fmt.Println("isCR")
-	return r == cr
-}
-
-func isLF(r rune) bool {
-	fmt.Println("isLF")
-	return r == lf
-}
-
-func isControl(r rune) bool {
-	if r == cr || r == zwj || r == zwnj {
-		return false
+func b2i(b bool) int {
+	if b {
+		return 1
 	}
-	return unicode.IsControl(r)
+	return 0
 }
 
-func isExtend(r rune) bool {
-	return unicode.In(r, unicode.Diacritic, unicode.Extender)
+func isOther(r rune) int {
+	fmt.Println("isOther")
+	return Other
 }
 
-func isRI(r rune) bool {
-	return unicode.Is(unicode.Regional_Indicator, r)
+func isCR(r rune) int {
+	fmt.Println("isCR")
+	return b2i(r == cr) * CR
 }
 
-func isPrepend(r rune) bool {
+func isLF(r rune) int {
+	fmt.Println("isLF")
+	return b2i(r == lf) * LF
+}
+
+func isControl(r rune) int {
+	if r == cr || r == zwj || r == zwnj {
+		return 0
+	}
+	return b2i(unicode.IsControl(r)) * Control
+}
+
+func isExtend(r rune) int {
+	return b2i(unicode.In(r, unicode.Diacritic, unicode.Extender)) * Extend
+}
+
+func isRI(r rune) int {
+	return b2i(unicode.Is(unicode.Regional_Indicator, r)) * RI
+}
+
+func isPrepend(r rune) int {
 	if unicode.In(r,
 	unicode.Prepended_Concatenation_Mark,
 	Consonant_Preceding_Repha,
 	Consonant_Prefixed) {
-		return true
+		return Prepend
 	}
-	return false
+	return 0
 }
 
-func isSpacingMark(r rune) bool {
+func isSpacingMark(r rune) int {
 	if unicode.Is(Grapheme_SpacingMarkExtras, r) {
-		return true
+		return SpacingMark
 	} else if unicode.Is(unicode.Mc, r) && !unicode.Is(Grapheme_SpacingMarkExceptions, r) {
-		return true
+		return SpacingMark
 	}
-	return false
+	return 0
 }
 
-func isExtPict(r rune) bool {
-	return false
+func isExtPict(r rune) int {
+	return 0
 }
-func isExtend_ExtCccZwj(r rune) bool {
-	return false
+func isExtend_ExtCccZwj(r rune) int {
+	return 0
 }
-func isZWJ_ExtCccZwj(r rune) bool {
-	return false
+func isZWJ_ExtCccZwj(r rune) int {
+	return 0
 }
