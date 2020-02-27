516fcae6f9d699427653172da888290efdc9f3a8
Adrien <adrien@arccoza.com>
2020-02-27 16:56:37 +0900

    Update hangul fns

--- pkg/hangul/functions.go
+++ pkg/hangul/functions.go
@@ -5,7 +5,7 @@ import (
 )
 
 const (
-	L = 1 << iota
+	L = 1 << 8 << iota
 	V
 	T
 	LV
@@ -17,39 +17,46 @@ const (
 	None       = 0
 )
 
-func isL(r rune) bool {
-	return 0x1100 <= r && r <= 0xA97C && unicode.Is(Leading_Jamo, r)
-}
-
-func isV(r rune) bool {
-	return 0x1160 <= r && r <= 0xD7C6 && unicode.Is(Vowel_Jamo, r)
-}
-
-func isT(r rune) bool {
-	return 0x11A8 <= r && r <= 0xD7FB && unicode.Is(Trailing_Jamo, r)
-}
-
-func isLV(r rune) bool {
-	return unicode.Is(LV_Syllable, r)
-}
-
-func isLVT(r rune) bool {
-	return 0xAC01 <= r && r <= 0xD7A3 && unicode.Is(LVT_Syllable, r)
-}
-
-func SyllableType(r rune) int {
-	switch {
-	case isL(r):
-		return L
-	case isV(r):
-		return V
-	case isT(r):
-		return T
-	case isLV(r):
-		return LV
-	case isLVT(r):
-		return LVT
+func b2i(b bool) int {
+	if b {
+		return 1
 	}
-
-	return None
+	return 0
 }
+
+func IsL(r rune) int {
+	return b2i(0x1100 <= r && r <= 0xA97C && unicode.Is(Leading_Jamo, r)) * L
+}
+
+func IsV(r rune) int {
+	return b2i(0x1160 <= r && r <= 0xD7C6 && unicode.Is(Vowel_Jamo, r)) * V
+}
+
+func IsT(r rune) int {
+	return b2i(0x11A8 <= r && r <= 0xD7FB && unicode.Is(Trailing_Jamo, r)) * T
+}
+
+func IsLV(r rune) int {
+	return b2i(unicode.Is(LV_Syllable, r)) * LV
+}
+
+func IsLVT(r rune) int {
+	return b2i(0xAC01 <= r && r <= 0xD7A3 && unicode.Is(LVT_Syllable, r)) * LVT
+}
+
+// func SyllableType(r rune) int {
+// 	switch {
+// 	case isL(r):
+// 		return L
+// 	case isV(r):
+// 		return V
+// 	case isT(r):
+// 		return T
+// 	case isLV(r):
+// 		return LV
+// 	case isLVT(r):
+// 		return LVT
+// 	}
+
+// 	return None
+// }
