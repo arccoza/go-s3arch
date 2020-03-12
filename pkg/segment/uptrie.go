47edb82d09c873bcc76ccd66211180856f891660
Adrien <adrien@arccoza.com>
2020-03-12 13:51:34 +0900

    WIP nibble trie, tidy up

--- pkg/segment/uptrie.go
+++ pkg/segment/uptrie.go
@@ -27,6 +27,10 @@ func main() {
 	pp.Println(t)
 }
 
+type UPTrie struct{
+	root node
+}
+
 type node struct {
 	prefix []byte
 	props uint64
@@ -36,10 +40,6 @@ type node struct {
 }
 
 type edges []node
-
-type UPTrie struct{
-	root node
-}
 
 func NewUPTrie() *UPTrie {
 	return &UPTrie{root: node{edges: make(edges, 16)}}
@@ -52,65 +52,6 @@ func (t *UPTrie) Get(key []byte) *node {
 func (t *UPTrie) Put(key []byte, props uint64) {
 	t.root.put(&node{prefix: key, hmask: 0xF0, tmask: 0x0F, props: props})
 }
-
-// func (n *node) add(b *node) {
-// 	idx := b.first()
-// 	a := &n.edges[idx]
-// 	fmt.Println("---")
-// 	fmt.Println("edges.add 0: ", idx)
-
-// 	if a.prefix == nil {
-// 		fmt.Println("edges.add 1: ")
-// 		n.edges[idx] = *b
-// 		return
-// 	}
-
-// 	if len(b.prefix) < len(a.prefix) {
-// 		*b, *a = *a, *b
-// 		if a.edges == nil {
-// 			a.edges = make(edges, 16)
-// 		}
-// 	}
-
-// 	brk, brkMsk := a.comparePrefixes(b)
-
-// 	off := 0
-// 	if brkMsk != 0xF0 {
-// 		off = 1
-// 	}
-
-// 	if brk < len(a.prefix) {
-// 		c := &node{prefix: a.prefix[brk + off:], hmask: ^brkMsk, tmask: a.tmask, props: a.props}
-// 		a.prefix = a.prefix[:brk + 1]
-// 		a.props, a.tmask = 0, brkMsk
-// 		a.add(c)
-// 	} else if brk == len(a.prefix) {
-// 		a.props |= b.props
-// 		return
-// 	}
-
-// 	b.prefix = b.prefix[brk + off:]
-// 	b.hmask = ^brkMsk
-// 	a.add(b)
-// 	fmt.Println("edges.add 4: ", brk, brkMsk, a, b)
-
-// }
-
-// func (a *node) comparePrefixes(b *node) (int, byte) {
-// 	brk, msk := 0, byte(0x0F)
-// 	for minLen := minInt(len(a.prefix), len(b.prefix)); brk < minLen; brk++ {
-// 		if ai, bi := a.prefix[brk], b.prefix[brk]; ai != bi {
-// 			if (ai ^ bi) & 0xF0 == 0 {
-// 				msk = 0xF0
-// 			} else {
-// 				brk--
-// 			}
-// 			break
-// 		}
-// 	}
-
-// 	return brk, msk
-// }
 
 func (e edges) add(n *node) {
 	idx := getNibble(n.prefix[0], n.hmask)
@@ -220,38 +161,14 @@ func getNibble(b, m byte) byte {
 	return n
 }
 
+func minInt(a, b int) int {
+	if a < b { return a }
+	return b
+}
+
 type Index [2]int
 func (i *Index) Get(bs []byte) byte {
 	x := i[1] % 2
 	m := byte((1 - x) * 0x0F + x * 0xF0)
 	return getNibble(bs[i[0]], m)
 }
-
-// func (n *node) first() byte {
-// 	f := n.prefix[0] & n.hmask
-// 	if f > 0x0F {
-// 		f = f >> 4
-// 	}
-// 	return f
-// }
-
-// func (n *node) last() byte {
-// 	l := n.prefix[len(n.prefix) - 1] & n.tmask
-// 	if l > 0x0F {
-// 		l = l >> 4
-// 	}
-// 	return l
-// }
-
-func minInt(a, b int) int {
-	if a < b { return a }
-	return b
-}
-
-// func nibble(b, m byte) byte {
-// 	if m == 0xF0 {
-// 		return b >> 4
-// 	} else {
-// 		return b & m
-// 	}
-// }
